34adcde0f047168fb28fd512d0ffb7bc00a1c3de
Hichem <hichembenzair@gmail.com>
2024-11-20 17:41:52 -0500

    Initial Commit

---
+++ src/main/java/com/cclean/ccleanws/CcleanWsApplication.java
@@ -0,0 +1,13 @@
+package com.cclean.ccleanws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcleanWsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CcleanWsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/cclean/ccleanws/CcleanWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cclean.ccleanws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcleanWsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
